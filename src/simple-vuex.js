612583cb0ed23cd171385efeab3f9949269f3773
鲍泽楠 <coxherbert98@gmail.com>
2025-08-13 17:30:56 +0800

    fix: vuex

--- src/simple-vuex.js
+++ src/simple-vuex.js
@@ -1,14 +1,16 @@
-import { reactive } from 'vue'
+import { reactive } from 'vue';
 
 export function createStore(options) {
-  const state = reactive(options.state())
-  const store = {
-    state,
-    commit(type, payload) {
-      const m = options.mutations && options.mutations[type]
-      if (m) m(state, payload)
-    }
-  }
-  store.install = (app) => { app.config.globalProperties.$store = store }
-  return store
+    const state = reactive(options.state);
+    const store = {
+        state,
+        commit(type, payload) {
+            const m = options.mutations && options.mutations[type];
+            if (m) m(state, payload);
+        },
+    };
+    store.install = (app) => {
+        app.config.globalProperties.$store = store;
+    };
+    return store;
 }
