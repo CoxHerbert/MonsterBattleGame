7abe63dfa9a383b45e4c0358fc5ff06acb03930b
鲍泽楠 <coxherbert98@gmail.com>
2025-08-14 14:40:21 +0800

    fix: route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import GameView from '@/views/GameView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import GameView from '@/views/GameView.vue';
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  // /game?mode=ENDLESS | PROGRESSION & chapterId=1-1
-  { path: '/game', name: 'game', component: GameView }
-]
+    { path: '/', name: 'home', component: HomeView },
+    // /game?mode=ENDLESS | PROGRESSION & chapterId=1-1
+    { path: '/game', name: 'game', component: GameView },
+];
 
-export default createRouter({ history: createWebHistory(), routes })
+export default createRouter({ history: createWebHashHistory('/MonsterBattleGame/'), routes });
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-const isPages = process.env.GITHUB_PAGES === 'true'
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-const base = isPages && !repo.endsWith('.github.io') ? `/${repo}/` : '/'
+const isPages = process.env.GITHUB_PAGES === 'true';
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const base = isPages && !repo.endsWith('.github.io') ? `/${repo}/` : '/';
 
 export default defineConfig({
-  plugins: [vue()],
-  base,
-  server: { host: '0.0.0.0', port: 5173, open: true },
-  resolve: { alias: { '@': '/src', 'vuex': '/src/simple-vuex.js' } }
-})
+    plugins: [vue()],
+    base: '/MonsterBattleGame/',
+    server: { host: '0.0.0.0', port: 5173, open: true },
+    resolve: { alias: { '@': '/src', vuex: '/src/simple-vuex.js' } },
+});
